157141537718fb162ca234b6c6cee3911dc3e17f
Cristian0703 <cristianmejia0703@gmail.com>
2022-01-14 19:10:02 -0400

    Resto de botones al form inicio y creacion de otros formularios

--- FACT-SC/inicio.Designer.cs
+++ FACT-SC/inicio.Designer.cs
@@ -30,16 +30,23 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(57, 115);
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(111, 115);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 57);
+            this.button1.Size = new System.Drawing.Size(106, 78);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Usuarios";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label1
             // 
@@ -51,11 +58,71 @@
             this.label1.TabIndex = 2;
             this.label1.Text = "Bienvenido al sistema";
             // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(346, 115);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(106, 78);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Artículos";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(564, 115);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(106, 78);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Facturación";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(564, 252);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(106, 78);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Reportería";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(346, 252);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(106, 78);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "Parametrización";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.Location = new System.Drawing.Point(111, 252);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(106, 78);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "Cotización";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
             // inicio
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(804, 441);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
@@ -71,5 +138,10 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
     }
 }
--- FACT-SC/inicio.cs
+++ FACT-SC/inicio.cs
@@ -16,5 +16,48 @@ namespace FACT_SC
         {
             InitializeComponent();
         }
+ 
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            usuarios formulario = new usuarios();
+            formulario.Show();
+            this.Hide();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            articulos formulario = new articulos();
+            formulario.Show();
+            this.Hide();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            facturacion formulario = new facturacion();
+            formulario.Show();
+            this.Hide();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            cotizacion formulario = new cotizacion();
+            formulario.Show();
+            this.Hide();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            parametrizacion formulario = new parametrizacion();
+            formulario.Show();
+            this.Hide();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            reporteria formulario = new reporteria();
+            formulario.Show();
+            this.Hide();
+        }
     }
 }
